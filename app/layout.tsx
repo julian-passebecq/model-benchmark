77a9c9087ece3145b9d42abad95c6fb90755ce6d
Julian Passebecq <64646018+julian-passebecq@users.noreply.github.com>
2026-09-24 15:31:39 +0200

    Set Fluent as default application theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="fluent">
       <body>{children}</body>
     </html>
   );
